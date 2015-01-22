b358314e9fed3e0e2a63fb14359d62dd52793cbf
Florian Lopes <florian.lopes@outlook.com>
2015-01-22 18:36:30 +0100

    Added EvaluationServiceTests

---
+++ src/main/java/com/ingesup/java/qcm/builder/EvaluationBuilder.java
@@ -0,0 +1,51 @@
+package com.ingesup.java.qcm.builder;
+
+import com.ingesup.java.qcm.entity.Evaluation;
+import com.ingesup.java.qcm.entity.EvaluationStudent;
+import com.ingesup.java.qcm.entity.Grade;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by lopes_f on 1/22/2015.
+ * <florian.lopes@outlook.com>
+ */
+public class EvaluationBuilder implements Builder<Evaluation> {
+
+	private Evaluation evaluation;
+
+	public EvaluationBuilder() {
+		this.evaluation = new Evaluation();
+	}
+
+	public EvaluationBuilder startDate(final Date startDate) {
+		this.evaluation.setStartDate(startDate);
+		return this;
+	}
+
+	public EvaluationBuilder endDate(final Date endDate) {
+		this.evaluation.setEndDate(endDate);
+		return this;
+	}
+
+	public EvaluationBuilder students(final List<EvaluationStudent> students) {
+		this.evaluation.setStudents(students);
+		return this;
+	}
+
+	public EvaluationBuilder grade(final Grade grade) {
+		this.evaluation.setGrade(grade);
+		return this;
+	}
+
+	public EvaluationBuilder id(final String id) {
+		this.evaluation.setId(id);
+		return this;
+	}
+
+	@Override
+	public Evaluation build() {
+		return null;
+	}
+}
--- src/main/java/com/ingesup/java/qcm/service/impl/AnswerServiceImpl.java
+++ src/main/java/com/ingesup/java/qcm/service/impl/AnswerServiceImpl.java
@@ -5,11 +5,13 @@ import com.ingesup.java.qcm.repository.AnswerRepository;
 import com.ingesup.java.qcm.repository.BaseRepository;
 import com.ingesup.java.qcm.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by lopes_f on 1/22/2015.
  * <florian.lopes@outlook.com>
  */
+@Service
 public class AnswerServiceImpl extends BaseServiceImpl<Answer, String> implements AnswerService {
 
 	private AnswerRepository answerRepository;
